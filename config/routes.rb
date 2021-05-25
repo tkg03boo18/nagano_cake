370e731f6bcd51cd926269b8b1ae0ebd8dbb39fa
tkg03boo18 <tkg03boo18@gmail.com>
2021-05-25 14:34:00 +0000

    [Start]エンドユーザーページ作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,24 @@
 Rails.application.routes.draw do
 
+  devise_for :customers, controllers: {
+    sessions: "customers/sessions",
+    registrations: "customers/registrations",
+    passwords: "customers/passwords"
+  }
   devise_for :admins, path: 'admin', controllers: {
     sessions: "admins/sessions"
   }
-  
+
   namespace :admin do
-    root to: 'homes#top'
     resources :items, only: [:new, :create, :index, :show, :edit, :update]
     resources :genres, only: [:index, :create, :edit, :update]
   end
+  get 'admin' => 'admin/homes#top'
+
+  scope module: :public do
+    root to: 'homes#top'
+    get 'about' => 'home#about'
+  end
+
+
 end
