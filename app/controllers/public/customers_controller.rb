3e0f57639a65f664d4bc295cffb859538a5341de
tkg03boo18 <tkg03boo18@gmail.com>
2021-05-26 07:08:18 +0000

    [Start]管理者顧客情報画面作成

---
+++ app/controllers/public/customers_controller.rb
@@ -0,0 +1,24 @@
+class Public::CustomersController < ApplicationController
+
+  def show
+    @customer = current_customer
+  end
+
+  def edit
+    @customer = current_customer
+  end
+
+  def update
+    @customer = current_customer
+    @customer.update(customer_params)
+    redirect_to customers_mypage_path
+  end
+
+  private
+
+  def customer_params
+  		params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :address, :postal_code, :telephone_number, :email, :is_deleted)
+  end
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  devise_for :customers, controllers: {
+  devise_for :customers, path: 'customer', controllers: {
     sessions: "customers/sessions",
     registrations: "customers/registrations",
     passwords: "customers/passwords"
@@ -12,12 +12,17 @@ Rails.application.routes.draw do
   namespace :admin do
     resources :items, only: [:new, :create, :index, :show, :edit, :update]
     resources :genres, only: [:index, :create, :edit, :update]
+    resources :customers, only: [:index, :show, :edit, :update]
   end
   get 'admin' => 'admin/homes#top'
 
   scope module: :public do
     root to: 'homes#top'
     get 'about' => 'home#about'
+    get 'customers/mypage' => 'customers#show'
+    get 'customers/edit' => 'customers#edit'
+    patch 'customers' => 'customers#update'
+    put 'customers' => 'customers#update'
   end
 
 
