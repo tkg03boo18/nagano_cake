7ce8a60130bfe616c2d096b1a2bf720d8b8c7976
tkg03boo18 <tkg03boo18@gmail.com>
2021-05-27 05:36:01 +0000

    [Start]コントローラーファイル作成

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,7 +1,7 @@
 class Order < ApplicationRecord
 
   belongs_to :customer
-  belongs_to :add
+  belongs_to :address
   has_many :order_details, dependent: :destroy
 
   enum payment_method: {クレジットカード: 0, 銀行振込: 1}
